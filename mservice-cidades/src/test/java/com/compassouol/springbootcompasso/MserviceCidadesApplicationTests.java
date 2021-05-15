a3087b49be534b3fdf5c765625d1c7e5dd127d04
thiagobcar <thiagobcar@gmail.com>
2021-05-15 12:06:30 -0300

    [spring-boot-compasso-1] - Criação do projeto de cadastro de cidades.

---
+++ mservice-cidades/src/test/java/com/compassouol/springbootcompasso/MserviceCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassouol.springbootcompasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
